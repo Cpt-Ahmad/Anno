570289a96a30fbc96f25149d7c7b0294df1d18aa
Ahmad Haidari <a.schoha@gmail.com>
2018-09-06 12:44:12 +0200

    Initial push

---
+++ desktop/src/de/cptahmad/anno/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.cptahmad.anno.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.cptahmad.anno.MainGameClass;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGameClass(), config);
+	}
+}
